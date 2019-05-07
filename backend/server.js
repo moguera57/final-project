1b88140bdcbc41aa90e1dc0c69b88bf3a3b235bf
moguera57 <josephbanwart@gmail.com>
2019-05-07 19:14:43 -0400

    heroku fix

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,14 @@ let Todo = require('./todo.model.js');
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 mongoose.connect('mongodb://development:thisisatest1@ds119984.mlab.com:19984/heroku_7th1x771', { useNewUrlParser: true });
 const connection = mongoose.connection;
 
